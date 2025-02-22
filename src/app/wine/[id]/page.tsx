5295fb62a9fe77c2c32f758ba47a265f81737053
CaoThuLuDau <npl.working@gmail.com>
2025-02-23 01:37:43 +0700

    Update: hot fix GetServerSideProps

--- src/app/wine/[id]/page.tsx
+++ src/app/wine/[id]/page.tsx
@@ -2,10 +2,18 @@ import { notFound } from "next/navigation";
 import wines from "@/data/wines"; // Mock data
 import Image from "next/image";
 import Button from "@/components/Button";
+import { GetServerSideProps } from "next";
 
 interface WineDetailProps {
     params: { id: string };
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const { id } = context.params;
+    return {
+        props: { params: { id } },
+    };
+};
 
 export default function WineDetail({ params }: WineDetailProps) {
     const wine = wines.find((w) => w.id.toString() === params.id);
@@ -14,11 +22,11 @@ export default function WineDetail({ params }: WineDetailProps) {
 
     return (
         <div className="container mx-auto p-6">
-            <Image 
-                src={wine.image} 
-                alt={wine.title} 
-                width={500} 
-                height={288} 
+            <Image
+                src={wine.image}
+                alt={wine.title}
+                width={500}
+                height={288}
                 className="w-full h-72 object-cover rounded-md" />
             <h1 className="text-3xl font-bold mt-4">{wine.title}</h1>
             <p className="text-gray-600 mt-2">{wine.description}</p>
